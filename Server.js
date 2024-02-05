e64bc8dd2359b63ec7c1e4417cb37c6eceb35e2f
Vaibhav Yadav <tron21vaibhav@gmail.com>
2024-02-05 15:48:47 +0530

    Added cluster module

--- Server.js
+++ Server.js
@@ -9,31 +9,31 @@ require('dotenv').config();
 app.use(express.json());
 // Bring the service file here and apply the changes
 require('./Services/cache');
-
-
 const authrouter = require('./Router/AuthRouter');
-app.use('/auth', authrouter);
-
-
-
-// Handle the route
-// This One For FileUpload}
 const router = require('./Router/NoteRouter')
 const AuthMiddleware = require('./MiddleWare/Authorize')
+
+
+// Cluster Module
+const cluster = require('cluster')
+cluster.schedulingPolicy = cluster.SCHED_RR;
+app.use('/auth', authrouter);
 app.use('/api', AuthMiddleware, router);
 
 
- 
-
-
-const PORT = keys.PORT;
-const { ConnectDB } = require("./ConnecttoMongoDB/Connection")
-const StartServer = async () => {
-    try {
-        await ConnectDB(keys.MONGOURL)
-        app.listen(PORT, console.log(`Server is Listening on PORT...... ${PORT}`))
-    } catch (err) {
-        console.log(err.message);
+if (cluster.isMaster) {
+    cluster.fork();
+} else {
+    console.log("Now, I'm Child Process");
+    const PORT = keys.PORT;
+    const { ConnectDB } = require("./ConnecttoMongoDB/Connection")
+    const StartServer = async () => {
+        try {
+            await ConnectDB(keys.MONGOURL)
+            app.listen(PORT, console.log(`Server is Listening on PORT...... ${PORT}`))
+        } catch (err) {
+            console.log(err.message);
+        }
     }
+    StartServer();
 }
-StartServer();
