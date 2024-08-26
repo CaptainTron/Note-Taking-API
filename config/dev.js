e73e7a2c32316cc11ee6e8d14af033b8ac1314cc
Vaibhav Yadav <94986377+CaptainTron@users.noreply.github.com>
2024-08-27 00:31:52 +0530

    Updated

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    PORT: 5000,
-    MONGOURL: "mongodb+srv://vaibhavwateam:AvNd0ggx9rcEiz2C@cluster0.yu0dfpl.mongodb.net/Note_Taking?retryWrites=true&w=majority",
-    JWT_EXPIRES_IN: "5d",
-    JWT_SECRET_KEY: "ThisIsDevServer",
-    accessKeyId: 'AKIARFPSHFLDGQ2OBABQ',
-    secretaccessKey: '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'
-}
